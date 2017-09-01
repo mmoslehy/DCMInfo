1a817672398469ad06fade7e997976b52254956d
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2017-09-01 12:43:54 -0400

    Throw error and exit when no input is specified

--- src/dcminfo.py
+++ src/dcminfo.py
@@ -1,6 +1,9 @@
 import dicom, subprocess, sys, os
 from tempfile import NamedTemporaryFile
 
+if len(sys.argv) < 2:
+  print("No input detected, usage: %s [file]" % sys.argv[0])
+  sys.exit(2)
 filepath = sys.argv[1]
 ds = dicom.read_file(filepath)
 filename = os.path.splitext(os.path.basename(filepath))[0]
